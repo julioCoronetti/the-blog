00067c6e0a02a37afac149238207964d981408bd
Julio Coronetti <timeset13@outlook.com>
2025-10-02 18:06:11 -0300

    Refactor layout and components for improved structure; add Container component, update Header, and adjust loading simulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import { postRepository } from "@/repositories/post/json-post-repository";
 import { Suspense } from "react";
 
 export default async function HomePage() {
-
   return (
-    <div>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">header</h1>
-      </header>
+    <Container>
+      <Header />
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -18,6 +16,6 @@ export default async function HomePage() {
       <footer>
         <p className="text-6xl font-bold text-center py-8">footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
-'use client';
-
 export function Header() {
-    return (
-        <h1>a</h1>
-    );
+  return (
+    <header>
+      <h1
+        className="text-4xl/normal font-extrabold py-8
+      sm:text-5xl sm:py-10
+      md:text-6xl md:py-11
+      lg:text-7xl lg:py-12"
+      >
+        <a href="#">The Blog</a>
+      </h1>
+    </header>
+  );
 }
